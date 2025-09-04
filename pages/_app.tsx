41a743b021a80f187c8ee15af4082ca8f7418436
edjimenezs <ed.jimenez.saa@gmail.com>
2025-09-04 10:02:07 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 import { SocketProvider } from '../contexts/SocketContext'
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SocketProvider>
       <Component {...pageProps} />
